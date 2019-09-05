917c5ae90368fd175f9ae649df321a39a2b090d3
galvanize1 <galvanize1@fvfx1fvyj1wl.ci.org>
2019-09-05 13:52:27 -0600

    step5 save the selected color

--- draw.js
+++ draw.js
@@ -1,3 +1,12 @@
 document
 .getElementById('canvas')
 .addEventListener('click', event => event.target.style.backgroundColor = 'blue')
+
+document
+.getElementById('palette')
+.addEventListener('click', event => {
+    const selectedColor = window.getComputedStyle(event.target, null).getPropertyValue("background-color");
+    console.log(selectedColor);
+    document.getElementById('currentColor').style.backgroundColor = selectedColor
+
+})
